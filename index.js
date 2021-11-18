c66e108a7c2ccd85d9625e8ad0861ff9519e4d51
Camila Faria <faria.camila@live.com>
2021-11-18 16:58:17 -0300

    video

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function area(){
+function aarea(){
     var video = document.querySelector('.video-img')
     video.innerHTML = `<iframe class="video-iframe" width="100%" src="https://www.youtube.com/embed/k0ELiXylCiE?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 }
